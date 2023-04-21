ebd278c649e0b6d4cb15f5cb33ab0f0b5975eedd
cjohan <cjohan@student.ethz.ch>
2023-04-21 09:46:14 +0200

    Updated and documented player_manager (#21)

--- src/server/player_manager.cpp
+++ src/server/player_manager.cpp
@@ -3,35 +3,35 @@
 #include "player_manager.h"
 
 // Initialize static map
-std::unordered_map<std::string, Player*> PlayerManager::_players_lut = {};
+std::unordered_map<std::string, Player*> PlayerManager::players = {};
 
 bool PlayerManager::try_get_player(const std::string& player_id, Player *&player_ptr) {
     player_ptr = nullptr;
-    _rw_lock.lock_shared();
-    auto it = PlayerManager::_players_lut.find(player_id);
-    if (it != _players_lut.end()) {
+    rw_lock.lock_shared();
+    auto it = PlayerManager::players.find(player_id);
+    if (it != players.end()) {
         player_ptr = it->second;
     }
-    _rw_lock.unlock_shared();
+    rw_lock.unlock_shared();
     return player_ptr;
 }
 
-bool PlayerManager::add_or_get_player(std::string name, const std::string& player_id, Player *&player_ptr) {
+bool PlayerManager::add_or_get_player(const std::string& player_id, Player *&player_ptr) {
     if (try_get_player(player_id, player_ptr)) {
         return true;
     }
-    player_ptr = new Player(player_id, name);
-    _rw_lock.lock();    // exclusive
-    PlayerManager::_players_lut.insert({player_id, player_ptr});
-    _rw_lock.unlock();
+    player_ptr = new Player(player_id);
+    rw_lock.lock();    // exclusive
+    PlayerManager::players.insert({player_id, player_ptr});
+    rw_lock.unlock();
     return true;
 }
 
 bool PlayerManager::remove_player(const std::string& player_id, Player *&player) {
     if (try_get_player(player_id, player)) {
-        _rw_lock.lock();    // exclusive
-        int nof_removals = PlayerManager::_players_lut.erase(player_id);
-        _rw_lock.unlock();
+        rw_lock.lock();    // exclusive
+        PlayerManager::players.erase(player_id);
+        rw_lock.unlock();
         return true;
     }
     return false;
--- src/server/player_manager.h
+++ src/server/player_manager.h
@@ -6,17 +6,38 @@
 #include <shared_mutex>
 #include <unordered_map>
 
-#include "../common/player.h"
+#include "../common/game_state/player.h""
 
+/**
+ * @brief Handles player management during a game instance.
+ */
 class PlayerManager {
 
 private:
 
-    inline static std::shared_mutex _rw_lock;
-    static std::unordered_map<std::string, Player*> _players_lut;
+    inline static std::shared_mutex rw_lock;
+    static std::unordered_map<std::string, Player*> players;
 
 public:
+    /**
+     * @brief Retrieves a player form the player list.
+     * 
+     * @param player_id 
+     * @param player_ptr 
+     */
     static bool try_get_player(const std::string& player_id, Player*& player_ptr);
-    static bool add_or_get_player(std::string name, const std::string& player_id, Player*& player_ptr);
+    /**
+     * @brief Adds new player to the player list or retrieves the existing player if present.
+     * 
+     * @param player_id 
+     * @param player_ptr 
+     */
+    static bool add_or_get_player(const std::string& player_id, Player*& player_ptr);
+    /**
+     * @brief Removes the player form the player list.
+     * 
+     * @param player_id 
+     * @param player 
+     */
     static bool remove_player(const std::string& player_id, Player*& player);
 };
