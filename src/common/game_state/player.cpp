09a1c00464f6f4cb1990d3b6a4f06a16e835a1fb
simon_eth <amenzi@student.ethz.ch>
2023-04-28 10:19:56 +0200

    Debugging

--- src/common/game_state/player.cpp
+++ src/common/game_state/player.cpp
@@ -1,10 +1,10 @@
 #include "include/player.h"
 
-Player::Player(unsigned short board_size, std::string id) : id(id) own_board(OwnBoard(board_size, 5)), enemy_board(EnemyBoard(board_size, 5)), is_prepared(false), is_ready(false){}
+// Player::Player(unsigned short board_size, std::string id) : id(id) own_board(OwnBoard(board_size, 5)), enemy_board(EnemyBoard(board_size, 5)), is_prepared(false), is_ready(false){}
 
-Player::Player(unsigned short board_size, unsigned short n_ships, std::string id) : id(id), own_board(OwnBoard(board_size, n_ships)), enemy_board(EnemyBoard(board_size, n_ships)), is_prepared(false), is_ready(false) {}
+// Player::Player(unsigned short board_size, unsigned short n_ships, std::string id) : id(id), own_board(OwnBoard()), enemy_board(EnemyBoard(board_size, n_ships)), is_prepared(false), is_ready(false) {}
 
-Player::Player(std::string id) : id(id), is_ready(false), is_prepared(false), own_board(OwnBoard(10, 5)), enemy_board(EnemyBoard(10, 5)) {}
+Player::Player(std::string id) : id(id), is_ready(false), is_prepared(false), own_board(OwnBoard()), enemy_board(EnemyBoard()) {}
 
 std::string Player::get_id(){
     return id;
--- src/common/main.cpp
+++ src/common/main.cpp
@@ -1,11 +1,13 @@
-#include "game_state/include/game_state.h"
-#include "game_state/include/player.h"
 #include "game_state/include/ship.h"
 #include "game_state/include/board.h"
+#include "game_state/include/player.h"
+#include "game_state/include/game_state.h"
 #include <cstdint>
 #include <cstdio>
 
 int main(){
     printf("Hello World from Common!\n");
+    // ShipCategory boat = Carrier;
+    // Ship my_ship(boat);
     return 0;
 }
