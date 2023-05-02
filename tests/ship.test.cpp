68acf381b75b7431f0dd9c8e3fb74ec74d34fefe
simon_eth <amenzi@student.ethz.ch>
2023-05-02 15:10:13 +0200

    repaired ship.test.cpp

--- tests/ship.test.cpp
+++ tests/ship.test.cpp
@@ -1,36 +1,35 @@
-// #include "ship.h"
+#include "ship.h"
 
-// #include "gtest/gtest.h"
+#include "gtest/gtest.h"
 
-// TEST(ShipTest, CreateShipDestroyer) {
-//     ShipCategory dest = Destroyer;
-//     Ship destroyer(dest);
-//     EXCEPT_EQ(0, destroyer.name);
-//     EXCEPT_EQ(0, Destroyer);
-//     EXCEPT_EQ(2, category_to_size(dest));
-//     EXCEPT_EQ(2, category_to_size(destroyer.name));
-// }
+TEST(ShipTest, CreateShipDestroyer) {
+    ShipCategory dest = Destroyer;
+    Ship destroyer(dest);
+    EXPECT_EQ(0, destroyer.get_name());
+    EXPECT_EQ(0, Destroyer);
+    EXPECT_EQ(2, category_to_size(dest));
+    EXPECT_EQ(2, category_to_size(destroyer.get_name()));
+}
 
-// TEST(ShipTest, ShootAtShip) {
-//     ShipCategory sub = Submarine;
-//     Ship my_ship(sub);
-//     EXCEPT_EQ(1, sub);
-//     EXCEPT_EQ(3, category_to_size(sub));
-//     EXCEPT_EQ(3, category_to_size(my_ship.name));
-//     EXCEPT_EQ(3, my_ship.remaining_length);
-//     EXCEPT_EQ(3, my_ship.length);
-//     EXCEPT_EQ(false, my_ship.is_sunk);
-//     my_ship.shot_at();
-//     EXCEPT_EQ(2, my_ship.remaining_length);
-//     EXCEPT_EQ(3, my_ship.length);
-//     EXCEPT_EQ(false, my_ship.is_sunk);
-//     my_ship.shot_at();
-//     EXCEPT_EQ(1, my_ship.remaining_length);
-//     EXCEPT_EQ(3, my_ship.length);
-//     EXCEPT_EQ(false, my_ship.is_sunk);
-//     my_ship.shot_at();
-//     EXCEPT_EQ(0, my_ship.remaining_length);
-//     EXCEPT_EQ(3, my_ship.length);
-//     EXCEPT_EQ(true, my_ship.is_sunk);
-//     EXCEPT_THROW(my_ship.shot_at(), std::exception);
-// }
+TEST(ShipTest, ShootAtShip) {
+    ShipCategory sub = Submarine;
+    Ship my_ship(sub);
+    EXPECT_EQ(1, sub);
+    EXPECT_EQ(3, category_to_size(sub));
+    EXPECT_EQ(3, category_to_size(my_ship.get_name()));
+    EXPECT_EQ(3, my_ship.get_damage());
+    EXPECT_EQ(3, my_ship.get_length());
+    EXPECT_EQ(false, my_ship.get_is_sunk());
+    my_ship.shot_at();
+    EXPECT_EQ(2, my_ship.get_damage());
+    EXPECT_EQ(3, my_ship.get_length());
+    EXPECT_EQ(false, my_ship.get_is_sunk());
+    my_ship.shot_at();
+    EXPECT_EQ(1, my_ship.get_damage());
+    EXPECT_EQ(3, my_ship.get_length());
+    EXPECT_EQ(false, my_ship.get_is_sunk());
+    my_ship.shot_at();
+    EXPECT_EQ(0, my_ship.get_damage());
+    EXPECT_EQ(3, my_ship.get_length());
+    EXPECT_EQ(true, my_ship.get_is_sunk());
+}
