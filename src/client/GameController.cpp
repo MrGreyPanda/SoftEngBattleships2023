e3a5239c29635caec8a2bff5aad6885efbdb64a5
mimina <mimina@student.ethz.ch>
2023-05-11 13:56:03 +0200

    Added GUI Library, removed comments from GameController.cpp

--- src/client/GameController.cpp
+++ src/client/GameController.cpp
@@ -5,13 +5,6 @@ void GameController::init()
     SDLGui::init("Battleships", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1080, 720, SDL_WINDOW_RESIZABLE, SDL_RENDERER_ACCELERATED);
     assert(SDLGui::SDLGuiCore::isInitialized() && "SDLGui is not initialized");
 
-    //SDLGui::SDLGuiContext* testContext = new SDLGui::SDLGuiContext("testContext");
-
-    //SDLGui::TextButtonWidget* testButton = new SDLGui::TextButtonWidget("testButton", "Test", .1f, .2f, .1f, .1f, 0., SDLGui::TextButtonFlagsExt_CenterText | SDLGui::TextButtonFlagsExt_CenterHorizontal);
-    //testContext->addWidget(testButton);
-
-    //SDLGui::SDLGuiEnvironment::pushContext(testContext);
-
     ConnectionWindow::init();
 }
 
@@ -26,12 +19,6 @@ void GameController::render()
     SDLGui::start();
     while (SDLGui::isRunning()) {
         SDLGui::newFrame();
-
-        //SDLGui::begin("testContext");
-
-        //if (SDLGui::TextButton("testButton")) printf("Test button pressed\n");
-
-        //SDLGui::end();
 
         ConnectionWindow::render();
 
