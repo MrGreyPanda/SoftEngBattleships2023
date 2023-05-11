b26c471df70e5a245e96af20aa17a3144c6c232c
mimina <mimina@student.ethz.ch>
2023-05-11 15:47:21 +0200

    Moved widgets around to try and match SRS

--- src/client/windows/GameWindow.cpp
+++ src/client/windows/GameWindow.cpp
@@ -8,15 +8,24 @@ void ConnectionWindow::init()
     SDLGui::SDLGuiContext* connectionWindowContext = 
         new SDLGui::SDLGuiContext("connectionWindow");
 
+    SDLGui::TextWidget* connectToServerText = 
+        new SDLGui::TextWidget("connectToServerText", "Connect to Server", 
+            .06f, .1f, .4f, .09f, 0., SDLGui::TextFlagsExt_CenterTextVertical);
+    connectionWindowContext->addWidget(connectToServerText);
+
+    SDLGui::TextWidget* serverAddressText = 
+        new SDLGui::TextWidget("serverAddressText", "Server Address", 
+            .1f, .4f, .3f, .06f, 0., SDLGui::TextFlagsExt_CenterText);
+    connectionWindowContext->addWidget(serverAddressText);
+
     SDLGui::TextInputWidget* serverAddressTextInput = 
         new SDLGui::TextInputWidget("serverAddressInput", m_serverAddress, 20,
-            0.f, .4f, .4f, .1f, 0., SDLGui::TextInputFlagsExt_CenterHorizontal |
-            SDLGui::TextInputFlagsExt_CenterText);
+            .55f, .4f, .4f, .06f, 0., SDLGui::TextInputFlagsExt_CenterText);
     connectionWindowContext->addWidget(serverAddressTextInput);
 
     SDLGui::TextButtonWidget* connectionButton = 
         new SDLGui::TextButtonWidget("serverConnectionButton", "Connect",
-            0.f, .51f, .3f, .1f, 0., SDLGui::TextButtonFlagsExt_CenterHorizontal |
+            0.f, .8f, .3f, .08f, 0., SDLGui::TextButtonFlagsExt_CenterHorizontal |
             SDLGui::TextButtonFlagsExt_CenterText);
     connectionWindowContext->addWidget(connectionButton);
 
