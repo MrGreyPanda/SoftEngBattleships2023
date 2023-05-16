852fdb00ac5bd20064593034424e46da6b47b8b1
mimina <mimina@student.ethz.ch>
2023-05-16 15:37:03 +0200

    Panel update

---
+++ include/preparation_panel.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+#include "SDLGui.hpp"
+
+#include "client_network_manager.h"
+
+class PreparationPanel {
+public:
+    static void init();
+
+    static void render();
+private:
+
+};
--- src/client/game_controller.cpp
+++ src/client/game_controller.cpp
@@ -1,9 +1,11 @@
 #include "game_controller.h"
 
+GameState GameController::game_state = GameState();
+
 void GameController::init() {
     SDLGui::init("Battleships", SDL_WINDOWPOS_UNDEFINED,
                  SDL_WINDOWPOS_UNDEFINED, 1080, 720,
-                 0,  // SDL_WINDOW_RESIZABLE
+                 SDL_WINDOW_RESIZABLE,
                  SDL_RENDERER_ACCELERATED);
     assert(SDLGui::SDLGuiCore::isInitialized() && "SDLGui is not initialized");
 
@@ -20,7 +22,7 @@ void GameController::render() {
     while (SDLGui::isRunning()) {
         SDLGui::newFrame();
 
-        ConnectionPanel::render();
+        if (game_state.get_phase() == Phase::Lobby) ConnectionPanel::render();
 
         SDLGui::renderFrame();
     }
