aa1a4a41ca607df2e400d898981ddaa649f584ad
Lukas Bühler <mail@lukasbuehler.ch>
2023-05-19 14:40:06 +0200

    added give up messages

--- src/common/network/messages/gave_up_message.cpp
+++ src/common/network/messages/gave_up_message.cpp
@@ -1,1 +1,17 @@
 #include "gave_up_message.h"
+
+GaveUpMessage::GaveUpMessage(const json& data) : GiveUpRequest(data) {}
+
+GaveUpMessage::GaveUpMessage(const std::string& game_id,
+                             const std::string& player_id)
+    : GiveUpRequest(game_id, player_id) {}
+
+json GaveUpMessage::to_json() const {
+    json j    = GiveUpRequest::to_json();
+    j["type"] = "gave_up_message";
+    return j;
+}
+
+MessageType GaveUpMessage::get_type() const {
+    return MessageType::GaveUpMessageType;
+}
---
+++ tests/network/requests/give_up_request.test.cpp
@@ -0,0 +1,30 @@
+#include "give_up_request.h"
+
+#include "gtest/gtest.h"
+#include "helper_functions.h"
+
+TEST(GiveUpRequest, JSONConstructor) {
+    json j = HelperFunctions::load_json_from_file(
+        "../json_examples/requests/give_up_request.json");
+
+    GiveUpRequest give_up_request(j);
+    EXPECT_EQ(give_up_request.get_type(), MessageType::GiveUpRequestType);
+    EXPECT_EQ(give_up_request.get_game_id(), "game_id");
+    EXPECT_EQ(give_up_request.get_player_id(), "player_id");
+}
+
+TEST(GiveUpRequest, Constructor) {
+    GiveUpRequest give_up_request("game_id", "player_id");
+    EXPECT_EQ(give_up_request.get_type(), MessageType::GiveUpRequestType);
+    EXPECT_EQ(give_up_request.get_game_id(), "game_id");
+    EXPECT_EQ(give_up_request.get_player_id(), "player_id");
+}
+
+TEST(GiveUpRequest, To_json) {
+    json j = HelperFunctions::load_json_from_file(
+        "../json_examples/requests/give_up_request.json");
+
+    GiveUpRequest give_up_request(j);
+
+    EXPECT_EQ(give_up_request.to_json(), j);
+}
