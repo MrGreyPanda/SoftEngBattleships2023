383fe38a99701ef40a690489a3cd2cd5f03b5ede
cjohan <cjohan@student.ethz.ch>
2023-05-21 19:04:35 +0200

    homogeneous buttons + ship sizes

--- src/client/panels/preparation_panel.cpp
+++ src/client/panels/preparation_panel.cpp
@@ -9,7 +9,7 @@ void PreparationPanel::init()
         new SDLGui::SDLGuiContext("preparation_panel_context");
 
     SDLGui::GridWidget* preparation_grid = 
-        new SDLGui::GridWidget("preparation_grid", .05f, .05f, .4f, .8f,
+        new SDLGui::GridWidget("preparation_grid", .07f, .05f, .4f, .8f,
         0., 10, 10, SDLGui::GridFlagsExt_CenterVertical | SDLGui::GridFlagsExt_Square);
     preparation_panel_context->addWidget(preparation_grid);
 
@@ -20,37 +20,38 @@ void PreparationPanel::init()
         preparation_panel_context->addWidget(preparation_phase_title);
 
     SDLGui::TextButtonWidget* ready_button = 
-        new SDLGui::TextButtonWidget("ready_button", "Ready", .0f, .9f, .3f, .08f, 0.,
-        SDLGui::TextButtonFlagsExt_CenterText | SDLGui::TextButtonFlagsExt_CenterHorizontal);
+        new SDLGui::TextButtonWidget("ready_button", "Ready", 0.f, .88f, .4f, .08f, 0.f,
+        SDLGui::TextButtonFlagsExt_CenterHorizontal |
+            SDLGui::TextButtonFlagsExt_CenterText);
     preparation_panel_context->addWidget(ready_button);
 
     SDLGui::DraggableImageWidget* carrier_ship = 
         new SDLGui::DraggableImageWidget("carrier_ship", "../assets/carrier.bmp",
-        .6f, .2f, .3f, .12f, 0., 5, 1, 
+        .6f, .2f, .3f, .11f, 0., 5, 1, 
         SDLGui::DraggableImageFlagsExt_CenterImage);
     preparation_panel_context->addWidget(carrier_ship);
     
     SDLGui::DraggableImageWidget* battleship_ship = 
         new SDLGui::DraggableImageWidget("battleship_ship", "../assets/battleship.bmp",
-        .6f, .35f, .3f, .12f, 0., 4, 1, 
+        .6f, .33f, .24f, .105f, 0., 4, 1, 
         SDLGui::DraggableImageFlagsExt_CenterImage);
     preparation_panel_context->addWidget(battleship_ship);
     
     SDLGui::DraggableImageWidget* cruiser_ship = 
         new SDLGui::DraggableImageWidget("cruiser_ship", "../assets/cruiser.bmp",
-        .6f, .5f, .3f, .12f, 0., 3, 1, 
+        .6f, .455f, .18f, .1f, 0., 3, 1, 
         SDLGui::DraggableImageFlagsExt_CenterImage);
     preparation_panel_context->addWidget(cruiser_ship);
     
     SDLGui::DraggableImageWidget* submarine_ship = 
         new SDLGui::DraggableImageWidget("submarine_ship", "../assets/submarine.bmp",
-        .6f, .65f, .3f, .12f, 0., 3, 1, 
+        .6f, .575f, .18f, .1f, 0., 3, 1, 
         SDLGui::DraggableImageFlagsExt_CenterImage);
     preparation_panel_context->addWidget(submarine_ship);
 
     SDLGui::DraggableImageWidget* destroyer_ship = 
         new SDLGui::DraggableImageWidget("destroyer_ship", "../assets/destroyer.bmp",
-        .6f, .8f, .3f, .12f, 0., 2, 1, 
+        .6f, .695f, .14f, .1f, 0., 2, 1, 
         SDLGui::DraggableImageFlagsExt_CenterImage);
     preparation_panel_context->addWidget(destroyer_ship);
 
